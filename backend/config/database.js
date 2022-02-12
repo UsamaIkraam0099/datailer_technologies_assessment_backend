75b72e10135a1726dee5448e1388e294330e4dc5
UsamaIkraam0099 <84959031+usamaikraam@users.noreply.github.com>
2022-02-12 20:40:13 +0400

    other

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 const connectDB = async () => {
   try {
-    const connection = await mongoose.connect(process.env.MONGO_URI);
+    const connection = await mongoose.connect(
+      "mongodb+srv://usama:usama12345@datailercluster.jqm50.mongodb.net/datailerapp?retryWrites=true&w=majority"
+    );
     console.log(`Mongo Database connected ${connection.connection.host}`);
   } catch (err) {
     console.log(`err ${err}`);
