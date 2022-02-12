a8084b3c14f771e66914ebec9e0c33a472db6da7
UsamaIkraam0099 <84959031+usamaikraam@users.noreply.github.com>
2022-02-12 22:11:35 +0400

    another commit

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,9 +1,17 @@
 const mongoose = require("mongoose");
-mongoose.Promise = global.Promise;
+// mongoose.Promise = global.Promise;
+
+const connectionParams = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+};
+
 const connectDB = async () => {
   try {
     const connection = await mongoose.connect(
-      "mongodb+srv://usama:usama12345@datailercluster.jqm50.mongodb.net/datailerapp?retryWrites=true&w=majority"
+      "mongodb+srv://usama:usama12345@datailercluster.jqm50.mongodb.net/datailerapp?retryWrites=true&w=majority",
+      connectionParams
     );
     console.log(`Mongo Database connected ${connection.connection.host}`);
   } catch (err) {
